e79b4837ddad17551cfa165d1aff7088f2c036a5
calvin9150 <wonbae9150@gmail.com>
2021-01-23 07:13:38 +0900

    First commit

---
+++ dev/test_controller/src/com/kh/practice/chap01/ControlPractice.java
@@ -0,0 +1,12 @@
+package com.kh.practice.chap01;
+
+import java.util.Scanner;
+
+public class ControlPractice {
+	Scanner sc = new Scanner(System.in);
+	
+	public void practice() {
+		
+	}
+	
+}
---
+++ dev/test_controller/src/com/kh/practice/chap01/Run.java
@@ -0,0 +1,12 @@
+package com.kh.practice.chap01;
+
+import com.kh.practice.chap01.ControlPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
